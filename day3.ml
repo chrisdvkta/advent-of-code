543ec0322171c826b761fc446e5e9ae1be6d5475
chrisdvkta <chrisdevkota77@gmail.com>
2024-12-08 21:15:20 +0545

    add:day3 init

---
+++ day3.ml
@@ -0,0 +1,17 @@
+(* 
+1. brute force approach 
+  find m, as soon as m is scanned, scan for the next set of characters ie ul(
+  if ^ succeeds then scan a number store it to x 
+  scan for , 
+  scan for another number, store it to y 
+  scan for )
+  success. 
+  add it to sum +=result
+
+2.optimal way
+  i got nothing. 
+*)
+
+
+
+
